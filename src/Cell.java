c1b779f3a1f986b654eaf33e2d15837143aabfae
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-04-19 15:09:13 +0200

    Added update metode

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,12 @@
 public class Cell {
     private int livingNeighbours;
     private boolean alive;
+
+    public String update() {
+        String strAlive = "dead";
+        if (alive) {
+            strAlive = "alive";
+        }
+        return strAlive;
+    }
 }
