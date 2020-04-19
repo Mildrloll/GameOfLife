a1aea364b508f8732a7ff13bb3a6ff7c581de1dc
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-04-19 15:11:36 +0200

    Made set and get metode

--- src/Cell.java
+++ src/Cell.java
@@ -9,4 +9,20 @@ public class Cell {
         }
         return strAlive;
     }
+
+    public int getLivingNeighbours() {
+        return livingNeighbours;
+    }
+
+    public void setLivingNeighbours(int livingNeighbours) {
+        this.livingNeighbours = livingNeighbours;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
 }
